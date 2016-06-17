12ca1155a52c4745763abc3ab6a4f7eeb0074728
Yuan Zhou <yuan.zhou@intel.com>
2016-06-17 10:53:30 +0800

    allow to remove entry
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- ufc.c
+++ ufc.c
@@ -66,8 +66,8 @@ int ufc_open(const char* path, const ufc_options_t* options, ufc_t** tufc)
             ufc_entry_meta->lba = -1;
             ufc_entry_meta->entry_size = 0;
             ufc_set_meta->emeta[j] = ufc_entry_meta;
+            ufc_set_meta->free_bits[j] = 0;
         }
-        ufc_set_meta->free_bits = 0;
         ufc->smeta[i] = ufc_set_meta;
     }
 
@@ -91,6 +91,22 @@ int get_inset_offset(ufc_t* ufc, uint64_t lba)
     return offs;
 }
 
+int ufc_lookup(ufc_t* ufc, int64_t lba)
+{
+
+    int set_id = get_target_set(ufc, lba);
+
+    int t_offset = -1;
+    int i;
+    for (i = 0; i < ufc->options->set_size ; i++) {
+        if (lba == ufc->smeta[set_id]->emeta[i]->lba) {
+            // hit
+            t_offset = set_id * (ufc->options->block_size * ufc->options->set_size) + ufc->options->block_size * i;
+        }
+    }
+
+    return t_offset;
+}
 
 int ufc_write(ufc_t* ufc, const void* log, size_t loglen, uint64_t lba)
 {
@@ -100,21 +116,29 @@ int ufc_write(ufc_t* ufc, const void* log, size_t loglen, uint64_t lba)
     }
 
     const char* p = (const char*) log;
-    
-    int set_id = get_target_set(ufc, lba);
 
-    int t_offset = -1;
-    int i;
-    for (i = 0; i < ufc->options->set_size ; i++) {
-        if (lba == ufc->smeta[set_id]->emeta[i]->lba) {
-            // find it -> re-dirty
-            t_offset = set_id * (ufc->options->block_size * ufc->options->set_size) + ufc->options->block_size * i;
+    int t_offset = ufc_lookup(ufc, lba);
+
+    if (-1 == t_offset) {
+        for (i = 0; i < ufc->options->set_size; i++) {
+            if (ufc->smeta[set_id]->free_bits[i] == 0) {
+                t_offset = set_id * (ufc->options->block_size * ufc->options->set_size) + ufc->smeta[set_id]->free_bits[i] * ufc->options->block_size;
+            }
+        }
+
+    }
+
+    if (-1 == t_offset) {
+        //TODO ufc_remove_set();
+        for (i=0; i<ufc->options->set_size; i++ ) {
+            ufc->smeta[set_id]->free_bits[i] = 0;
         }
     }
-    if (-1 == t_offset) {
-        //TODO full handling
-        t_offset = set_id * (ufc->options->block_size * ufc->options->set_size) + ufc->smeta[set_id]->free_bits * ufc->options->block_size;
 
+    for (i =0; i < ufc->options->set_size; i++) {
+        if (ufc->smeta[set_id]->free_bits[i] == 0) {
+            t_offset = set_id * (ufc->options->block_size * ufc->options->set_size) + ufc->smeta[set_id]->free_bits[i] * ufc->options->block_size;
+        }
     }
 
     //printf("set_id = %d, t_offset=%d\n", set_id, t_offset);
@@ -125,15 +149,8 @@ int ufc_write(ufc_t* ufc, const void* log, size_t loglen, uint64_t lba)
         return err;
     }
 
-    ufc->smeta[set_id]->emeta[ufc->smeta[set_id]->free_bits]->lba = lba;
-    ufc->smeta[set_id]->emeta[ufc->smeta[set_id]->free_bits]->entry_size = loglen;
-
-    if (ufc->smeta[set_id]->free_bits == (ufc->options->set_size - 2)) {
-        //TODO rewind: flush+evict
-        ufc->smeta[set_id]->free_bits = 0;
-    } else {
-        ufc->smeta[set_id]->free_bits += 1;
-    }
+    ufc->smeta[set_id]->emeta[ufc->smeta[set_id]->free_bits[i]]->lba = lba;
+    ufc->smeta[set_id]->emeta[ufc->smeta[set_id]->free_bits[i]]->entry_size = loglen;
 
     return 0;
   
@@ -144,18 +161,10 @@ int ufc_read(ufc_t* ufc, char* data, size_t loglen, uint64_t lba)
     int set_id = get_target_set(ufc, lba);
 
     void* p = (void*) data;
-    int t_offset = -1;
-    int i;
-    for(i = 0; i < ufc->options->set_size ; i++) {
-        if (lba == ufc->smeta[set_id]->emeta[i]->lba) {
-            // find it -> re-dirty
-            t_offset = set_id * (ufc->options->block_size * ufc->options->set_size) + ufc->options->block_size * i;
-        }
-    }
-    if (-1 == t_offset) {
-        //TODO full handling
-        t_offset = set_id * (ufc->options->block_size * ufc->options->set_size) + ufc->smeta[set_id]->free_bits * ufc->options->block_size;
+    int t_offset = ufc_lookup(ufc, lba)
 
+    if (-1 == t_offset) {
+        return 0;
     }
 
     lseek(ufc->fd, t_offset, SEEK_SET);
@@ -166,6 +175,22 @@ int ufc_read(ufc_t* ufc, char* data, size_t loglen, uint64_t lba)
     }
 
     return 0;
+}
+
+int ufc_remove(ufc_t* ufc, uint64_t lba)
+{
+    int set_id = get_target_set(ufc, lba);
+    int i;
+    for(i = 0; i < ufc->options->set_size ; i++) {
+        if (lba == ufc->smeta[set_id]->emeta[i]->lba) {
+            // read hit
+            ufc->smeta[set_id]->emeta[i]->lba = -1;
+            ufc->smeta[set_id]->free_bits[i] = 0;
+        }
+    }
+
+    return 0;
+
 }
 
 int ufc_sync(ufc_t* ufc) 
--- ufc.h
+++ ufc.h
@@ -40,7 +40,7 @@ extern "C"
   
   typedef struct ufc_set_meta_t {
       ufc_entry_meta_t *emeta[512];
-      int free_bits;
+      int free_bits[512];
   } ufc_set_meta_t;
   
   typedef struct ufc_t {
@@ -55,9 +55,11 @@ extern "C"
   void ufc_options_destroy(ufc_options_t* options);
   
   int ufc_open(const char* dir, const ufc_options_t* options, ufc_t** tufc);
+  int ufc_lookup(ufc_t* ufc, int64_t lba)
   int ufc_write(ufc_t* ufc, const void* log, size_t loglen, uint64_t lba);
   int ufc_read(ufc_t* ufc, char* data, size_t loglen, uint64_t lba);
   int ufc_sync(ufc_t* ufc);
+  int ufc_remove(ufc_t* ufc, int64_t lba)
   /*
   int ufc_meta_read(ufc_t* ufc, const int key);
   int ufc_meta_write(ufc_t* ufc, const int key, const int addr);
